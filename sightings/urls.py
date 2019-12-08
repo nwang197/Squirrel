43c3d9baa70c833fa01c76d0674648a7f01fe7e1
wc2695 <wc2695@columbia.edu>
2019-12-08 19:34:06 +0000

    add wrote sightings urls

--- sightings/urls.py
+++ sightings/urls.py
@@ -4,4 +4,9 @@ from . import views
 
 
 urlpatterns = [
-    path
+    path('/sightings',views.all_Ss),
+    path('/sightings/add', views.addsquirrel),
+    path('/sightings/<Unique_Squirrel_ID>/', views.squirrel_details),
+    path('/sightings/edit', views.edit_squirrel),
+    path('/sightings/stats',views.stats),
+            ]
