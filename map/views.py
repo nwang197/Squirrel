93bd3986ac0ac61fefb48bcc2b37e7ff2babe096
Nuoya Wang <nw2432@columbia.edu>
2019-12-08 06:47:14 +0000

    add map views

--- map/views.py
+++ map/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Squirrel
 
-# Create your views here.
+def plotmap(request):
+    pinpoints = Squirrel.objects.all()
+    context = {
+        'pinpoints': pinpoints
+        }
+    return render(request, 'map/map.html', context)
